b455e855516b1380861d101d491bbc08473d5d13
Sarah Heimlich <sarahheimlich@google.com>
2022-03-04 19:00:16 +1100

    Fix license
    Change-Id: I8a1aa7293b3044e37dc773bdb7145537370a4bd5

--- pkg/FontChooser/surface/allowlist.js
+++ pkg/FontChooser/surface/allowlist.js
@@ -1,8 +1,9 @@
 /**
- * @license
- * Copyright (c) 2022 Google LLC All rights reserved.
+ * Copyright 2022 Google LLC
+ * 
  * Use of this source code is governed by a BSD-style
- * license that can be found in the LICENSE file.
+ * license that can be found in the LICENSE file or at
+ * https://developers.google.com/open-source/licenses/bsd
  */
 // load helpers
 import {define, loadCss, loadScript} from 'https://arcsjs-apps.web.app/Library/Common/dom/dom.js';
--- pkg/demo/fonts/Library/SuggestedFonts.js
+++ pkg/demo/fonts/Library/SuggestedFonts.js
@@ -1,10 +1,11 @@
 /* globals decorator, filter html */
 ({
 /**
- * @license
- * Copyright (c) 2022 Google LLC All rights reserved.
+ * Copyright 2022 Google LLC
+ * 
  * Use of this source code is governed by a BSD-style
- * license that can be found in the LICENSE file.
+ * license that can be found in the LICENSE file or at
+ * https://developers.google.com/open-source/licenses/bsd
  */
 render({fonts}) {
   return {
