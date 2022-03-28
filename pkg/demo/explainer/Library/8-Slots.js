32dd0eee49c40b7a8b0be0a85356140e42a3786a
Ray Cromwell <cromwellian@google.com>
2022-03-27 20:19:50 -0700

    Initial version of explainer tutorial with samples for each step.

---
+++ pkg/demo/explainer/Library/8-Slots.js
@@ -0,0 +1,118 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * Code distributed by Google as part of this project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+/* globals log, html */
+({
+
+  render({fonts}) {
+    return {
+      fonts: {
+        models: fonts,
+        $template: 'font_t',
+        decorator: 'decorator',
+      }
+    };
+  },
+
+  decorator({family, fullName, weight, style, privateData}) {
+    const fweight = style.includes('Bold') ? 'bold' : weight;
+    const fstyle = style.includes('Italic') ? 'italic' : style.includes('Oblique') ? 'oblique' : '';
+    const favorite = privateData?.favorite || false;
+    return {
+      key: fullName,
+      sortKey: `family`,
+      name: fullName,
+      displayStyle: `font-family: "${family}"; font-weight: ${fweight}; font-style: ${fstyle};`,
+      privateData: {
+        favorite: favorite
+      }
+    };
+  },
+
+  onFontClick({eventlet: {key}}, {}) {
+    return {pickedFont: key};
+  },
+
+  onFavorite({eventlet: {value}}) {
+    value.favorite = true;
+  },
+
+  onUnfavorite({eventlet: {value}}) {
+    value.favorite = false;
+  },
+
+  template: html`
+    <style>
+      :host {
+        display: block;
+        height: 100%;
+        border: 1px solid #f3f3f3;
+        box-sizing: border-box;
+      }
+      [pickedFontBox] {
+        background-color: yellow;
+      }
+      [list] {
+        cursor: pointer;
+        padding: 12px;
+        box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+      }
+      [fonts] {
+        font-family: monospace, sans-serif;
+        font-size: 11px;
+      }
+      [font]:hover {
+        background-color: lightblue;
+      }
+      [toolbar] {
+        padding: 2px 8px 2px 0 !important;
+        line-height: 12px;
+      }
+      [name] {
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+      [banner] {
+        padding: 2px 8px;
+        border-top-left-radius: 4px;
+        border-top-right-radius: 4px;
+        background-color: lightgreen;
+        color: #333;
+        font-size: 9px;
+        font-weight: bold;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        line-height: 2em;
+      }
+      [banner] > * {
+        margin: 0 4px;
+      }
+    </style>
+
+    <div scrolling slot="boldfonts"></div>
+    <div banner>All Fonts</div>
+    <div scrolling fonts list>{{fonts}}</div>
+
+    <template favorite_t>
+      <div on-click="onFontClick" key="{{key}}">
+        <span>{{name}}</span>
+        <span style$="{{displayStyle}}">Sample</span>
+      </div>
+    </template>
+
+    <template font_t>
+      <div on-click="onFontClick" key="{{key}}">
+        <span>{{name}}</span>
+        <span style$="{{displayStyle}}">Sample</span>
+      </div>
+    </template>
+  `
+});
+
---
+++ pkg/demo/explainer/app-full.js
@@ -0,0 +1,82 @@
+import {FontChooser} from '../../FontChooser/FontChooser.js';
+import {fonts} from './CustomFontSet.js';
+
+// we need an absolute url to the location of the local library
+let local = `${window.location.href}`;
+if (local.endsWith('.html')) {
+  local = local.substring(0, local.lastIndexOf('/') + 1);
+}
+
+local = local + 'Library';
+
+// express button state
+const {b0} = window;
+const buttonsOn = trueToEnable => [b0].forEach(e => e.disabled = !trueToEnable);
+
+// two FontChooser request examples
+
+const SampleRequest = {
+  // custom recipe
+  kind: `${local}/LocalFontsRecipe`,
+  // custom fonts
+  webFonts: fonts,
+  // custom container
+  chooser: window.menu
+};
+
+const SimpleRequest = {
+  // custom recipe
+  kind: `${local}/LocalFontsRecipe`,
+  // custom fonts
+  webFonts: fonts,
+  // custom container
+  chooser: window.menu
+};
+
+const FamilyRequest = {
+  // custom recipe
+  kind: `${local}/FullDemo`,
+  // custom fonts
+  webFonts: fonts,
+  // custom container
+  chooser: window.chooser
+};
+
+// ui
+
+window.onFontsMenu = async () => {
+  doit(SimpleRequest);
+};
+
+window.onFontsPanel = async () => {
+  doit(FamilyRequest);
+};
+
+window.onSamplePanel = async (recipe) => {
+  SampleRequest.kind = `${local}/${recipe}Recipe`;
+  console.log(SampleRequest.kind);
+  doit(SampleRequest);
+};
+
+const doit = async request => {
+  setFont(await requestFont(request));
+};
+
+const requestFont = async request => {
+  buttonsOn(false);
+  return FontChooser.requestFont(request);
+};
+
+const setFont = font => {
+  buttonsOn(true);
+  console.warn('Chosen font:', font);
+  test.style.cssText = font && fontMetaToStyle(font);
+};
+
+const fontMetaToStyle = ({family, fullName, weight, style}) => {
+  const fweight = style.includes('Bold') ? 'bold' : weight;
+  const fstyle = style.includes('Italic') ? 'italic' : style.includes('Oblique') ? 'oblique' : '';
+  return `font-family: "${family}"; font-weight: ${fweight}; font-style: ${fstyle};`;
+};
+
+buttonsOn(true);
