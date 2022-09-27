3e7beec47de7b16f219ab9c987a7cc41eff17315
Ray Cromwell <cromwellian@gmail.com>
2022-09-27 11:00:22 -0700

    Workaround __dirname not in esm

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ import bodyParser from "body-parser";
 import fs from "fs";
 import tmp from "tmp";
 import { exec } from "child_process";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
